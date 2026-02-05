e900b01472f93179256e03cde6ea7ae1d3a7a365
John Agius <26434960+johnagius@users.noreply.github.com>
2026-02-05 21:02:55 +0100

    Create modules.temperature.js

---
+++ ui/modules.temperature.js
@@ -0,0 +1,9 @@
+(function(){
+  window.EIKON = window.EIKON || {};
+  window.EIKON.modules = window.EIKON.modules || {};
+  window.EIKON.modules.temperature = {
+    render: function(root){
+      root.innerHTML = '<div class="eikon-test"><b>Eikon UI loaded.</b><div>Next: load Temperature module UI here.</div></div>';
+    }
+  };
+})();
